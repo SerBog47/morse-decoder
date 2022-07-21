5a5723d19157ffaca117f32cd1bab87a2a5cea3d
Sergey Bogomolov <Bogomolov15@yandex.ru>
2022-07-21 17:58:44 +0300

    update: update index.js

--- src/index.js
+++ src/index.js
@@ -38,9 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const chars = expr.match(/.{1,10}/g);
+    let result = "";
+    chars.forEach(element => {
+        if (element === '**********') result += ' ';
+        let units = element.match(/.{1,2}/g);
+        let subResult = '';
+        units.forEach(element2 => {
+            if (element2 === '00') subResult += '';
+            if (element2 === '10') subResult += '.';
+            if (element2 === '11') subResult += '-';
+        });
+        for (const key in MORSE_TABLE) {
+            if (key === subResult) result += MORSE_TABLE[key];
+        }
+    });
+    return result;
 }
 
 module.exports = {
-    decode
+  decode
 }
